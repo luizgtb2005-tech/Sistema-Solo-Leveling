bca7c6da6dd96dd9a2d785073e4dac88081f520a
luizgtb2005-tech <luizgtb2005@gmail.com>
2026-01-15 11:32:23 -0300

    Atualização
    Agora da para escolher uma missão e subir de nivel

--- main.py
+++ main.py
@@ -10,17 +10,21 @@ class Player:
         self.intelligence = 10
 
     def status(self):
+        print("\n--- STATUS DO JOGADOR: ---")
         print(f"Nome: {self.name}")
         print(f"Nivel: {self.level}")
-        print(f"XP:{self.xp}")
+        print(f"XP:{self.xp}/{self.xp_to_level}")
         print(f"Força: {self.strength}")
         print(f"Agilidade: {self.agility}")
         print(f"Inteligencia: {self.intelligence}")
-        print("_" * 30)
+        print("--------------------------")
 
     def gain_xp(self, amount):
-        print(f"ganhou {amount} de XP")
+        print(f"\nGanhou {amount} de XP")
         self.xp += amount
+
+        while self.xp >= self.xp_to_level:
+            self.level_up()
 
         if self.xp >= self.xp_to_level:
             self.level_up()
@@ -34,13 +38,41 @@ class Player:
         self.agility += 2
         self.intelligence += 2
 
-        print("LEVEL UP")
+        print("\n🔥 LEVEL UP!")
         print(f"Agora você é nível {self.level}")
 
+class Quest:
+    def __init__(self, name, reward_xp):
+        self.name = name
+        self.reward_xp = reward_xp
+
+    def complete(self, player):
+        print(f"\nMissão concluída: {self.name}")
+        player.gain_xp(self.reward_xp)
+
+# ===========================
+# INÍCIO DO SISTEMA
+# ===========================
 
 player = Player("Caçador")
-print("Sistema Solo Leveling iniciado\n")
 
+quest_1 = Quest("Derrotar monstros fracos", 50)
+quest_2 = Quest("Limpar dungeon inicial", 120)
+
+print("Sistema Solo Leveling iniciado")
 player.status()
-player.gain_xp(120)
+
+print("\nEscolha uma missão:")
+print("1 - Derrotar monstros fracos:")
+print("2 - Limpar dungeon inicial:")
+
+choice = input("Digite o número da missão:")
+
+if choice == "1":
+    quest_1.complete(player)
+elif choice == "2":
+    quest_2.complete(player)
+else:
+    print("\nEscolha invalida. Nenhuma missão realizada.")
+
 player.status()
